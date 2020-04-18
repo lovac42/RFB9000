8189f23cf1e46051e33f989f83ae1e19db396f68
lovac42 <lovac42@protonmail.com>
2020-04-17 20:26:19 -0400

    Added version warning

--- src/rfb9000/__init__.py
+++ src/rfb9000/__init__.py
@@ -4,4 +4,6 @@
 # License: GNU GPL, version 3 or later; http://www.gnu.org/copyleft/gpl.html
 
 
+from .lib.com.lovac42.anki.others import safety_first
+
 from .main import *
--- src/rfb9000/const.py
+++ src/rfb9000/const.py
@@ -4,9 +4,14 @@
 # License: GNU GPL, version 3 or later; http://www.gnu.org/copyleft/gpl.html
 
 
+import os
+
 ADDON_NAME = "RFB9000"
 
 TITLE = "RFB-9000: A Google Translator"
 
 DEFAULT_HOTKEY = "Ctrl+Shift+G"
 
+ADDON_PATH = os.path.dirname(__file__)
+
+TARGET_STABLE_VERSION = 23
