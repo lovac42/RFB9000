6c03b5a836a522c5d9b451797b0219d7f64aa8c1
lovac42 <lovac42@protonmail.com>
2020-07-05 17:23:50 -0400

    bump version to v0.3.0

--- src/build.bat
+++ src/build.bat
@@ -2,7 +2,7 @@
 set ZIP=C:\PROGRA~1\7-Zip\7z.exe a -tzip -y -r
 set REPO=rfb9000
 set PACKID=rfb9000
-set VERSION=0.2.0
+set VERSION=0.3.0
 
 
 quick_manifest.exe "RFB9000: A Google Translator" "%PACKID%" >%REPO%\manifest.json
@@ -11,7 +11,11 @@ echo %VERSION% >%REPO%\VERSION
 fsum -r -jm -md5 -d%REPO% * > checksum.md5
 move checksum.md5 %REPO%\checksum.md5
 
-REM %ZIP% %REPO%_v%VERSION%_Anki20.zip *.py %REPO%\*
 cd %REPO%
 %ZIP% ../%REPO%_v%VERSION%_Anki21.ankiaddon *
 %ZIP% ../%REPO%_v%VERSION%_CCBC.adze *
+
+
+
+
+REM  Support for 2.0 in no longer possible due to changes made to the API which is PY3 specific.
