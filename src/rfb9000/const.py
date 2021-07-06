8f292ede3fb63934f1c995fcc03570eb299d03e0
lovac42 <lovac42@protonmail.com>
2021-07-06 18:59:26 -0400

    Updated copyright to 2021

--- src/rfb9000/const.py
+++ src/rfb9000/const.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright: (C) 2020 Lovac42
+# Copyright: (C) 2020-2021 Lovac42
 # Support: https://github.com/lovac42/RFB9000
 # License: GNU GPL, version 3 or later; http://www.gnu.org/copyleft/gpl.html
 
